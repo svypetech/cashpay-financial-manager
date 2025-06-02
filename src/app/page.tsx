363801f0cf9601441149cf5be5d4dcc64e3e9468
Muhammad Kabeer <muhammadkabeer2003@gmail.com>
2025-06-02 17:14:21 +0500

    Deleted Unnecessary page.tsx in (pages) folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 "use client";
-import { useEffect } from "react"
 
-export default function Home() {
-  useEffect(()=>{
-    window.location.href = "/dashboard"
-  },[])
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+export default function Page() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/dashboard");
+  }, []);
+
+  return (
+    <div className=" flex flex-col items-between justify-between h-screen"></div>
+  );
 }
